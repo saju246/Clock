57c9554725afed70361f2255cad8b2a8f23c0b47
saju246 <sajarudheensaju@gmail.com>
2024-03-22 14:16:45 +0530

    commit

---
+++ app.js
@@ -0,0 +1,22 @@
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+        let session  = "AM";
+        if(h==0){
+            h=12
+        }
+        if(h>12){
+            h-=12
+            session = "PM"
+        }
+      
+        h= (h<10)?"0"+h:h;
+        m= (m<10)?"0"+m:m;
+        s= (s<10)?"0"+s:s;
+    document.getElementById('digitalClock').innerHTML = h + ":" + m + ":" + s + "" + session;
+    setTimeout(showTime,1000)
+    }
+   showTime()
